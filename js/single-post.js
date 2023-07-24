5eaaaae88294ecaecc3a217f6388eb83cc0f1341
MariusAskelovic <marashz@gmail.com>
2023-07-24 10:31:42 +0300

    get query params

---
+++ js/single-post.js
@@ -0,0 +1,27 @@
+'use strict';
+console.log('single-post.js file was loaded');
+
+const els = {
+  title: document.getElementById('title'),
+  body: document.getElementById('body'),
+  tags: document.getElementById('tags'),
+};
+
+console.log('els ===', els);
+// paimti url parametra pavadinimu postId reiksme
+const params = new URLSearchParams(window.location.search);
+console.log('params ===', params);
+// ir ja isspausdinti
+console.log(params.get('postId'));
+
+// parsiusti konkretu posta pagal id
+const currentPostId = params.get('postId');
+const urlDummyPosts = 'https://dummyjson.com/posts/' + currentPostId;
+fetch(urlDummyPosts)
+  .then((resp) => resp.json())
+  .then((jsonObj) => {
+    console.log(jsonObj);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
