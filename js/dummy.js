5ae4d3d917d1037ba7b83b5fbf5b11848502b1cd
MariusAskelovic <marashz@gmail.com>
2023-07-24 13:09:48 +0300

    fiveUsers fetched

--- js/dummy.js
+++ js/dummy.js
@@ -32,12 +32,48 @@ function makePostLiEl(arr, destHtmlEl) {
       aEl.href = 'single-post.html?postId=' + postObj.id;
       aEl.textContent = postObj.id + '(id)' + postObj.title;
       // ==== sukurti delete mygtuka
+      const delBtn = document.createElement('button');
+      // prideti del mygtukui data-postId = id
+      delBtn.dataset.postId = postObj.id;
+      delBtn.textContent = 'X';
+      // prideti argumenta deletePostServer()
+      // tai as paverciu i arrow
+      delBtn.addEventListener('click', () => deletePostServer(postObj.id));
       // ideti i li elementa
       // uzdeti mygtukui funkcija deletePost()
 
-      liEl.append(aEl);
+      liEl.append(aEl, ' ', delBtn);
       console.log('liEl ===', liEl);
       return liEl;
     })
     .forEach((htmlEl) => destHtmlEl.append(htmlEl));
 }
+
+function deletePostServer(idToDelete) {
+  console.log('delete me ', idToDelete);
+  fetch(`https://dummyjson.com/posts/${idToDelete}`, {
+    method: 'DELETE',
+  })
+    .then((resp) => {
+      // log resp
+      return resp.json();
+    })
+    .then((ats) => {
+      //   console.log('ats ===', ats);
+      // trinti lokaliai
+      localDelete(idToDelete);
+    })
+    .catch((error) => console.log(error));
+}
+
+function localDelete(idToDelete) {
+  const foundBtn = document.querySelector(`[data-post-id='${idToDelete}']`);
+  //   console.log('found ===', foundBtn);
+  foundBtn.parentElement.remove();
+}
+
+const url5Users = 'https://dummyjson.com/users?limit=5';
+fetch(url5Users)
+  .then((resp) => resp.json())
+  .then((userObj) => console.log('users === ', userObj.users))
+  .catch((error) => console.log(error));
