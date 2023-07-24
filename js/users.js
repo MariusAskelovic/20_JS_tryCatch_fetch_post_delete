2f31cf886dd8ef83c7c79bfd027e2774919e58e2
MariusAskelovic <marashz@gmail.com>
2023-07-24 14:38:02 +0300

    sorting with button

--- js/users.js
+++ js/users.js
@@ -1,5 +1,7 @@
 'use strict';
 console.log('users.js file was loaded');
+const ulEl = document.getElementById('ulList');
+const sortBtnEl = document.getElementById('sortUsers');
 
 const url5Users = 'https://dummyjson.com/users?limit=5';
 fetch(url5Users)
@@ -7,14 +9,18 @@ fetch(url5Users)
   .then((oneObj) => {
     const userObj = oneObj.users;
     console.log('userObj ===', userObj);
-    makeUsersLiEl(usersSortedByName(userObj));
+    makeUsersLiEl(userObj);
+    // makeUsersLiEl(usersSortedByName(userObj));
+    sortBtnEl.addEventListener('click', () => {
+      ulEl.innerHTML = '';
+      makeUsersLiEl(usersSortedByName(userObj));
+    });
   })
   .catch((error) => console.log(error));
 
 function makeUsersLiEl(arr) {
   console.log('arr ===', arr);
   arr.map((oneUserObj) => {
-    const ulEl = document.createElement('ul');
     const liEl = document.createElement('li');
     const fNameAndLNameEl = document.createElement('h3');
     const genderAndAgeEl = document.createElement('h4');
@@ -31,6 +37,7 @@ function makeUsersLiEl(arr) {
     emailEl.href = `mailto: ${oneUserObj.email}`;
 
     liEl.append(fNameAndLNameEl, genderAndAgeEl, addressEl, phoneEL, emailEl);
+    console.log('liEl ===', liEl);
     ulEl.append(liEl);
     document.body.append(ulEl);
   });
